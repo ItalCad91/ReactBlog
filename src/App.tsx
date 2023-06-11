c53e7e3b25e270569825db8413096370a838b455
ItalCad91 <riccardoreali05@gmail.com>
2023-06-11 15:36:18 -0400

    navigation update

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,21 @@ import Navbar from './Components/Navbar/Navbar'
 import AboutPage from './Components/AboutPage'
 import HomePage from './Components/HomePage'
 import ContactPage from './Components/ContactPage'
-import {Route, Routes} from "react-router-dom"
-
+import { Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <div>
       <Navbar/>
-      <div>
-        <Routes>
-          <Route path = "/Home" element = {<HomePage/>}/>
-          <Route path = "/About" element = {<AboutPage/>}/>
-          <Route path = "/Contact" element = {<ContactPage/>}/>
-        </Routes>  
-      </div> 
-    </div>    
-       
-  )
+      <Routes>
+        <Route path="/" element={<HomePage />} index />
+        <Route path="/Home" element={<HomePage />} />
+        <Route path="/About" element={<AboutPage />} />
+        <Route path="/Contact" element={<ContactPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
